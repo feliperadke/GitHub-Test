b4b40cf21f221dae38b78433cf6d6d2071ecb771
aagustini <alexandre.agustini@pucrs.br>
2018-08-13 15:12:39 -0300

    Código inicial do exercício de revisão

--- src/br/pucrs/ep/poo/App.java
+++ src/br/pucrs/ep/poo/App.java
@@ -12,10 +12,10 @@ public class App {
 		c1.move(2,3);
 		c2.zoom(2);
 		
-		System.out.println("Circulo 1: "+c1.toString());
+		System.out.println("\nCirculo 1: "+c1.toString());
 		System.out.println("Circulo 2: "+c2.toString());
 		
-		System.out.println("Area do circulo 1: "+c1.area());
+		System.out.println("\nArea do circulo 1: "+c1.area());
 		System.out.println("Area do circulo 2: "+c2.area());
 		
 	}
